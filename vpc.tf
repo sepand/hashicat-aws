20355b630e3135efdec512517618eb3c89fdce81
sepand <mr.sepand@yahoo.com>
2020-03-11 16:54:46 -0700

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
+}
