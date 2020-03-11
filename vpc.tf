912cc9ee3736470accc52d6c87ba3758eee07573
sepand <mr.sepand@yahoo.com>
2020-03-11 16:58:57 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main_zz" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_vpc" "main_zz" {
+resource "aws_vpc" "main_zzy" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
 
